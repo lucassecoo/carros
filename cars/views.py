24fb6eb9974287221a54c1631cd780c824359724
Lucas Seco Alves <lucasseco46.2@gmail.com>
2025-09-16 20:31:18 -0300

    registrando carros no banco

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cars.models import Car, Brand
+from cars.forms import CarForm
 
 def cars_view(request):
-    return render(
+    search = request.GET.get('search')
+    cars = Car.objects.all().order_by('model')
+
+    if search:
+        cars = cars.filter(model__icontains=search)#filtra pelo modelo do veiculo
+
+    return render( #renderiza na url
         request,
         'cars.html',
-        {'cars': {'model':'Astra 2.0'}},
+        {'cars': cars}, #cars remete a classe Car dentro do banco de dados
+    )
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarForm()
+    return render(
+        request,
+        'new_car.html',
+        {'new_car_form': new_car_form}
     )
