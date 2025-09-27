a44d4ac4e9b34f93000190d0714ebd2fdac8065b
Lucas Seco Alves <lucasseco46.2@gmail.com>
2025-09-27 16:07:53 -0300

    validando form

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from cars.views import cars_view, new_car_view, new_brand_view
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
